b2e9c357215544e046fac2f5219ebf415d82c8b1
katlego modupi <katlegomodupi@protonmail.com>
2024-08-29 11:39:01 +0200

    lsp

---
+++ lua/nugetlsp-nvim/init.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+function M.setup(opts)
+  opts = opts or {}
+
+  local client = vim.lsp.start_client {
+    name = 'nugetlsp',
+    cmd = { 'node', '/home/katlego/source/nugetlsp/dist/index.js', '--stdio' },
+  }
+
+  if not client then
+    print 'Failed to start nugetlsp server'
+  else
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+      pattern = '*.csproj',
+      callback = function()
+        vim.lsp.buf_attach_client(0, client)
+      end,
+    })
+  end
+end
+
+return M
